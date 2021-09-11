b291c8cd7b6d8e742a6dc4633a76b66b302dc107
Rattenjunge <les.dams96@gmail.com>
2021-09-11 17:16:21 +0200

    [ADDED] Drag&Drop

---
+++ SomethingSomethingCardGame/Assets/Scripts/DropZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 position = new Vector2();
+
+    private void Start()
+    {
+        
+    }
+}
+
